bfdacf78ae1da417e1a9281654d40a1645393348
Takeaki23 <takeaki.sakakibara@students.iaac.net>
2022-03-08 17:37:20 +0900

    using 2

--- src/examples/3d_iphone_case/script.js
+++ src/examples/3d_iphone_case/script.js
@@ -9,7 +9,7 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
 const data = {
-  definition: '3d_iphone_case.gh',
+  definition: '3d_iphone_case2.gh',
   inputs: getInputs()
 }
 
